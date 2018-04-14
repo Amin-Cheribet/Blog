074b1b951184c78168f6e477be7f735a8efdf2ca
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-04-14 15:38:15 +0100

    admin template (layout)

---
+++ App/Views/template/admin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="/css/admin.css">
+        @yield('head')
+    </head>
+    <body>
+        <div class="menu">
+
+        </div>
+        <div class="content">
+            @yield('body')
+        </div>
+    </body>
+</html>
