ee81473274cee514e231dfc94712881a73ccce16
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-04-19 13:55:57 +0100

    admin sidebar

---
+++ App/Views/include/admin-sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="sidebar">
+    <a href="{{url('/')}}">
+        <div class="element">Visit Website</div>
+    </a>
+    <a href="{{url('admin/posts')}}">
+        <div class="element">Posts</div>
+    </a>
+    <a href="{{url('admin/statistics')}}">
+        <div class="element">Statistics</div>
+    </a>
+    <a href="{{url('admin/users')}}">
+        <div class="element">Members</div>
+    </a>
+    <a href="admin/configuration">
+        <div class="element">Configuration</div>
+    </a>
+</div>
