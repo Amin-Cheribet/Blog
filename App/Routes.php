1d73465ee713a1ba1382481298f5f9ac5599a9bc
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-09-02 21:55:45 -0400

    improve code quality

--- App/Routes.php
+++ App/Routes.php
@@ -1,10 +1,10 @@
 <?php
 
-$route->get('/', function() {
+$route->get('/', function () {
     view('index');
 });
 
-$route->get('language/{$language}', function($language) {
+$route->get('language/{$language}', function ($language) {
     language($language);
     redirect(previousUrl());
 });
@@ -13,23 +13,23 @@ $route->get('loadgrid/{$offset}/{$number}', 'Post@grid');
 
 $route->resource('comment', 'Comment');
 
-$route->group('post', function($route) {
+$route->group('post', function ($route) {
     $route->get('translate/{$id}', 'Post@translate');
     $route->post('translate/{$id}', 'Post@storeTranslate');
     $route->get('delete/{$id}', 'Post@delete');
     $route->resource('/', 'Post');
 });
 
-$route->group('user', function($route) {
+$route->group('user', function ($route) {
     $route->post('login', 'User@login');
-    $route->get('disconnect','User@disconnect');
+    $route->get('disconnect', 'User@disconnect');
     $route->get('block/{$id}', 'User@block');
     $route->get('unblock/{$id}', 'User@unBlock');
     $route->get('delete/{$id}', 'User@delete');
     $route->resource('/', 'User');
 });
 
-$route->group('admin', function($route) {
+$route->group('admin', function ($route) {
     $route->get('/', 'Admin@index');
     $route->get('posts/', 'Admin@postsList');
     $route->get('users', 'Admin@usersList');
