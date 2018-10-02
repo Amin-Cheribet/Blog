34b024bd9840e449e8892dd5605edc5217e62bd3
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-10-02 14:04:41 -0400

    Add SEO feature + improve code quality

--- App/Controllers/Admin.php
+++ App/Controllers/Admin.php
@@ -27,34 +27,63 @@ class Admin extends Controller
 
     public function showConfiguration()
     {
-        $data = file_get_contents(rootDir().'/config.json');
-        view('admin/configuration', ['data' => json_decode($data)]);
+        view('admin/configuration', ['data' => $this->getConfiguration()]);
     }
 
     public function updateConfiguration(Request $request)
     {
-        $coverImage = new Upload('coverimage');
-        $coverImage->validate()->number(1);
-        $request->validate('name', 'name')->required();
-        $errors = array_merge($request->getErrors(), $coverImage->getErrors());
+        $request->validate('name', 'Blog Name')->required();
+        $request->validate('languages', 'Languages')->required();
+        $request->validate('comments', 'Comments')->required();
+        $request->validate('visits', 'Visits visibility')->required();
 
-        if (!empty($errors)) {
-            return view('admin/configuration', ['data' => $request, 'errors' => $errors]);
+        if (!empty($request->getErrors())) {
+            return view('admin/configuration', ['data' => $this->getConfiguration(), 'errors' => $request->getErrors()]);
+        }
+        $configuration = $this->getConfiguration();
+        $data = [
+            'name'      => $request->name,
+            'languages' => $request->languages,
+            'comments'  => $request->comments,
+            'visits'    => $request->visits,
+            'seo'       => $request->seo,
+        ];
+        $data['coverImage'] = $configuration->coverImage;
+
+        $this->updateConfigurationFile($data);
+
+        route('admin/configuration');
+    }
+
+    public function updateCoverImage(Request $request)
+    {
+        $coverImage = new Upload('coverimage');
+        $coverImage->validate()->number(1)->extension(['png','jpg','jpeg']);
+
+        if (!empty($coverImage->getErrors())) {
+            return view('admin/configuration', ['data' => $this->getConfiguration(),'uploadErrors' => $coverImage->getErrors()]);
         }
 
         $coverImage = $coverImage->save('./Storage');
-        $data = [
-            'name'       => $request->name,
-            'languages'  => $request->languages,
-            'comments'   => $request->comments,
-            'visits'     => $request->visits,
-            'coverImage' => $coverImage[0]->path,
-        ];
+
+        $configuration = $this->getConfiguration(true);
+        $configuration['coverImage'] = $coverImage->path;
+        $this->updateConfigurationFile($configuration);
+
+        return view('admin/configuration', ['data' => $this->getConfiguration()]);
+    }
+
+    private function getConfiguration($flag = false)
+    {
+        $json = file_get_contents(rootDir().'config.json');
+        return json_decode($json, $flag);
+    }
+
+    private function updateConfigurationFile(array $data)
+    {
         if (!file_put_contents(rootDir().'/config.json', json_encode($data))) {
             throw new \Exception("Faild to update configuration", 1);
         }
-
-        route('admin/configuration');
     }
 
     public function statistics()
